92491f01f886d53ad91592c1efdbf49f2a350492
Dmitriy Shatalov <deman4ik@gmail.com>
2018-04-06 20:46:04 +0300

    Vote for Link

--- hackernews-node/src/resolvers/Subscription.js
+++ hackernews-node/src/resolvers/Subscription.js
@@ -11,6 +11,20 @@ const newLink = {
     subscribe: newLinkSubscribe
 };
 
+function newVoteSubscribe(parent, args, context, info) {
+    return context.db.subscription.vote(
+        // https://github.com/graphcool/prisma/issues/1734
+        // { where: { mutation_in: ['CREATED'] } },
+        {},
+        info
+    );
+}
+
+const newVote = {
+    subscribe: newVoteSubscribe
+};
+
 module.exports = {
-    newLink
+    newLink,
+    newVote
 };
