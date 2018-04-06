62c413cf607b3079aca3218aa196465a30594f8e
Dmitriy Shatalov <deman4ik@gmail.com>
2018-04-06 20:14:41 +0300

    Realtime GraphQL Subscription - on new Link created

---
+++ hackernews-node/src/resolvers/Subscription.js
@@ -0,0 +1,16 @@
+function newLinkSubscribe(parent, args, context, info) {
+    return context.db.subscription.link(
+        // https://github.com/graphcool/prisma/issues/1734
+        // { where: { mutation_in: ['CREATED'] } },
+        {},
+        info
+    );
+}
+
+const newLink = {
+    subscribe: newLinkSubscribe
+};
+
+module.exports = {
+    newLink
+};
